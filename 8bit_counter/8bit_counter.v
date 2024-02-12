9fa22eb03107e278e1a95da6f1c3b7269e70cea4
Hariprasath-hub <143018676+Hariprasath-hub@users.noreply.github.com>
2024-02-12 12:58:36 +0530

    Update 8bit_counter.v

--- 8bit_counter/8bit_counter.v
+++ 8bit_counter/8bit_counter.v
@@ -1,1 +1,27 @@
+timescale 1ns / 1ps
+module counter(
+input wire clk,
+    input wire reset,
+    input wire load,
+    input wire [7:0] preset,
+    output wire tcount
+
+    );
+    reg [7:0] counter;
+
+always @(posedge clk or posedge reset)
+begin
+    if (reset)
+        counter <= 8'h00;
+    else if (load)
+        counter <= preset;
+    else if (counter == 8'hFF)
+        counter <= 8'h00;
+    else
+        counter <= counter + 1;
+end
+
+assign tcount = (counter == 8'hFF);
+
+endmodule
 
